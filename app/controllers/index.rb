6ebf0b1b6e747dd86af2ee0112753173e49c8605
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 18:56:22 -0500

    Login functionality is working! The functionality of creating a new account needs to be tackled next.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,18 +12,28 @@ end
 
 get '/secret_recipe' do
   erb :secret_recipe
+  # || erb :index
+end
+
+post '/secret_recipe' do
+  erb :secret_recipe
 end
 
 post '/login' do
-  User.f
-end
-
-post '/user_page' do
-  current_user = User
-  erb :user_page
+  @user = User.find_by_email(params[:existing_email])
+  if @user != nil
+    passed = User.authenticate(params[:existing_email], params[:existing_password])
+    if passed
+       return erb :secret_recipe
+    else
+      erb :index
+    end
+  else
+    erb :index
+  end
 end
 
 post '/new_account' do
   @user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
-  erb :user_page
+  erb :index
 end
