488590e55904a738d3054db5fa253edf6daf33ff
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 16:49:11 -0500

    Created secret recipe file with baked student.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,6 +10,10 @@ get '/new_account' do
   erb :new_account
 end
 
+get '/secret_recipe' do
+  erb :secret_recipe
+end
+
 post '/new_account' do
   @user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
   erb :user_page
